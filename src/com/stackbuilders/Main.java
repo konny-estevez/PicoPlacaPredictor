8e88e17304a1455618e9a320838287375295d8e3
KonnyDenysse <denyss.estevez@gmail.com>
2020-04-12 01:26:56 -0500

    Adding basic validations on inputs

--- src/com/stackbuilders/Main.java
+++ src/com/stackbuilders/Main.java
@@ -10,13 +10,25 @@ public class Main {
 
         // Scanner object
         Scanner inputText = new Scanner(System.in);
-        System.out.println("Pico & Placa Predicotr");
+        System.out.println("Pico & Placa Predictor");
         System.out.println();
-        System.out.println("Please enter a license plate number");
-        licensePlate = inputText.nextLine();
-        System.out.println("Please enter the date");
-        date = inputText.nextLine();
-        System.out.println("Please enter the time");
-        time = inputText.nextLine();
+        do {
+            System.out.println("Please enter a license plate number (format: Pxx0000)");
+            licensePlate = inputText.nextLine();
+            if (licensePlate.length() != 7)
+                System.out.println("Incorrect license plate, please enter a valid value.");
+        } while (licensePlate.length() != 7);
+        do {
+            System.out.println("Please enter the date (format: yyyy/mm/dd)");
+            date = inputText.nextLine();
+            if (date.length() != 10)
+                System.out.println("Incorrect date, please enter a valid value.");
+        } while (date.length() != 10);
+        do {
+            System.out.println("Please enter the time (format: HH:MM)");
+            time = inputText.nextLine();
+            if (time.length() != 5)
+                System.out.println("Incorrect time, please enter a valid value.");
+        } while (time.length() != 5);
     }
 }
