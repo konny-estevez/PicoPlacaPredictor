b1c97b55d744c0ddcb4a046928850eb078d8d921
KonnyDenysse <denyss.estevez@gmail.com>
2020-04-12 19:24:33 -0500

    Adding advanced validations on inputs with regular expressions

--- src/com/stackbuilders/Main.java
+++ src/com/stackbuilders/Main.java
@@ -1,34 +1,61 @@
 package com.stackbuilders;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Variables
-        String licensePlate, date, time;
-
+        String licensePlate, dateString, timeString;
+        StringBuilder errorMessage;
+        LocalDate date;
+        LocalTime time;
         // Scanner object
         Scanner inputText = new Scanner(System.in);
+        // Validator object
+        Validators validator = new Validators();
+        errorMessage = new StringBuilder();
+
         System.out.println("Pico & Placa Predictor");
         System.out.println();
         do {
-            System.out.println("Please enter a license plate number (format: Pxx0000)");
+            System.out.println("Please enter a license plate number (format: xxx0000)");
             licensePlate = inputText.nextLine();
-            if (licensePlate.length() != 7)
-                System.out.println("Incorrect license plate, please enter a valid value.");
-        } while (licensePlate.length() != 7);
+            errorMessage.delete(0, errorMessage.length());
+            licensePlate = validator.ValidateLicensePlate(licensePlate, errorMessage);
+            if (licensePlate == null) {
+                System.out.println("Error: " + errorMessage);
+                System.out.println("Incorrect license plate, please enter a valid format and value.");
+                System.out.println();
+            }
+        } while (licensePlate == null);
+        System.out.println();
         do {
-            System.out.println("Please enter the date (format: yyyy/mm/dd)");
-            date = inputText.nextLine();
-            if (date.length() != 10)
-                System.out.println("Incorrect date, please enter a valid value.");
-        } while (date.length() != 10);
+            System.out.println("Please enter the date (format: yyyy-mm-dd)");
+            dateString = inputText.nextLine();
+            errorMessage.delete(0, errorMessage.length());
+            date = validator.ValidateDate(dateString, errorMessage);
+            if (date == null) {
+                System.out.println("Error: " + errorMessage);
+                System.out.println("Incorrect date, please enter a valid format and value.");
+                System.out.println();
+            }
+        } while (date == null);
+        System.out.println();
         do {
             System.out.println("Please enter the time (format: HH:MM)");
-            time = inputText.nextLine();
-            if (time.length() != 5)
-                System.out.println("Incorrect time, please enter a valid value.");
-        } while (time.length() != 5);
+            timeString = inputText.nextLine();
+            errorMessage.delete(0, errorMessage.length());
+            time = validator.ValidateTime(timeString, errorMessage);
+            if (time == null) {
+                System.out.println("Error: " + errorMessage);
+                System.out.println("Incorrect time, please enter a format and valid value.");
+                System.out.println();
+            }
+        } while (time == null);
+        System.out.println();
+        System.out.println("Process terminated...");
     }
 }
---
+++ src/com/stackbuilders/Validators.java
@@ -0,0 +1,59 @@
+package com.stackbuilders;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Validators {
+    // Vraiables
+    private static String licensePlateExpression;
+    private static Pattern licensePlatePattern;
+    private static Character[] provinceLetters = { 'A', 'B', 'U', 'C', 'X', 'H', 'O', 'E', 'W', 'G', 'I', 'L', 'R', 'M', 'V', 'N', 'S', 'P', 'Q', 'K', 'T', 'Z', 'Y', 'J'};
+
+    // Constructor
+    Validators() {
+        licensePlateExpression = "^\\(?([A-Z]{3})\\)?([0-9]{4})$";
+        licensePlatePattern = Pattern.compile(licensePlateExpression);
+    }
+
+    // License Plate Validation Method
+    String ValidateLicensePlate(String licensePlate, StringBuilder errorMessage) {
+        String result = null;
+        licensePlate = licensePlate.trim();
+        Matcher m = licensePlatePattern.matcher(licensePlate);
+        if (m.find()) {
+            if (!Arrays.asList(provinceLetters).contains(licensePlate.charAt(0)))
+                errorMessage.append("Not valid province letter on License Plate");
+            else
+                result = licensePlate;
+        }
+        else
+            errorMessage.append("License Plate not match");
+        return  result;
+    }
+
+    // Date Validation Method
+    LocalDate ValidateDate(String dateString, StringBuilder errorMessage) {
+        try {
+            return LocalDate.parse(dateString);
+        }
+        catch (Exception ex) {
+            errorMessage.append(ex.getMessage());
+        }
+        return null;
+    }
+
+    // Time Validation Method
+     LocalTime ValidateTime(String timeString, StringBuilder errorMessage) {
+        LocalTime result = null;
+        try {
+            return LocalTime.parse(timeString);
+        }
+        catch (Exception ex) {
+            errorMessage.append(ex.getMessage());
+        }
+        return null;
+    }
+}
